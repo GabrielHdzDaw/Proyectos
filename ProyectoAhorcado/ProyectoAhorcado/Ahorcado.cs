b18c9078dec43d71a405c8028110984fc0edc10f
gabrielhdzdaw <gabrielhernandezdaw@gmail.com>
2025-02-18 14:00:09 +0100

    18/2/2025

---
+++ ProyectoAhorcado/ProyectoAhorcado/Ahorcado.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoAhorcado
+{
+    internal class Ahorcado
+    {
+        List<string> palabras = new List<string>();
+        int palabra;
+        string estado;
+        int vidas;
+        int maxVidas;
+        bool ganado;
+        public Ahorcado(List<string> palabras)
+        {
+            this.palabras = palabras;
+            palabra = 0;
+            estado = "";
+            vidas = 0;
+            maxVidas = 6;
+            ganado = false;
+        }
+
+        public List<string> GetPalabras() { return palabras; }
+        public int GetPalabra() { return palabra; }
+        public string GetEstado()
+        {
+            string res = "";
+            switch(vidas)
+            {
+                case 0:
+                    break;
+                case 1:
+                    res = "cabeza";
+                    break;
+                case 2:
+                    res = "torso";
+                    break;
+                case 3:
+                    res = "brazoizq";
+                    break;
+                case 4:
+                    res = "brazoder";
+                    break;
+                case 5:
+                    res = "piernaizq";
+                    break;
+                case 6:
+                    res = "piernader";
+                    break;
+                default:
+                    break;
+            }
+            return res;
+        }
+        public int GetVidas() { return vidas; }
+
+        public void SetVidas(int vidas) { this.vidas = vidas; }
+        public void SetPalabras(List<string> palabras) { this.palabras = palabras; }
+        public void SetPalabra(int palabra) { this.palabra = palabra; }
+        public void Add(string palabra) { palabras.Add(palabra); }
+        public void AddDaño() { vidas++; }
+
+        public void DibujarPalabra()
+        {
+            Console.SetCursorPosition((Console.WindowWidth / 2), (Console.WindowHeight / 2) + 10);
+            Console.WriteLine("Palabra: " + estado);
+        }
+
+        public void DibujarAhorcado(string estado)
+        {
+            Console.SetCursorPosition((Console.WindowWidth / 2) - 2, (Console.WindowHeight / 2) - 1);
+
+            Console.WriteLine("____");
+            Console.SetCursorPosition((Console.WindowWidth / 2) - 2, (Console.WindowHeight / 2) + 1);
+            Console.WriteLine("|");
+            Console.SetCursorPosition((Console.WindowWidth / 2) - 2, (Console.WindowHeight / 2) + 2);
+            Console.WriteLine("|");
+            Console.SetCursorPosition((Console.WindowWidth / 2) - 2, (Console.WindowHeight / 2) + 3);
+            Console.WriteLine("|");
+            Console.SetCursorPosition((Console.WindowWidth / 2) - 2, (Console.WindowHeight / 2) + 4);
+            Console.WriteLine("_");
+            Console.SetCursorPosition(Console.WindowWidth / 2, Console.WindowHeight / 2);
+            switch (estado)
+            {
+                case "cabeza":
+                    Console.Write("  O\n");
+                    break;
+                case "torso":
+                    Console.Write("  O\n");
+                    Console.SetCursorPosition(Console.WindowWidth / 2, (Console.WindowHeight / 2) + 1);
+                    Console.Write("  |\n");
+                    break;
+                case "brazoizq":
+                    Console.Write("  O\n");
+                    Console.SetCursorPosition(Console.WindowWidth / 2, (Console.WindowHeight / 2) + 1);
+                    Console.Write(" /|\n");
+                    break;
+                case "brazoder":
+                    Console.Write("  O\n");
+                    Console.SetCursorPosition(Console.WindowWidth / 2, (Console.WindowHeight / 2) + 1);
+                    Console.Write(" /|\\\n");
+                    break;
+                case "piernaizq":
+                    Console.Write("  O\n");
+                    Console.SetCursorPosition(Console.WindowWidth / 2, (Console.WindowHeight / 2) + 1);
+                    Console.Write(" /|\\\n");
+                    Console.SetCursorPosition(Console.WindowWidth / 2, (Console.WindowHeight / 2) + 2);
+                    Console.Write(" /\n");
+                    break;
+                case "piernader":
+                    Console.Write("  O\n");
+                    Console.SetCursorPosition(Console.WindowWidth / 2, (Console.WindowHeight / 2) + 1);
+                    Console.Write(" /|\\\n");
+                    Console.SetCursorPosition(Console.WindowWidth / 2, (Console.WindowHeight / 2) + 2);
+                    Console.Write(" / \\\n");
+                    break;
+            }
+        }
+
+        public bool HasGanado()
+        {
+            return estado == palabras[palabra];
+        }
+
+        public bool EstasAhorcado()
+        {
+            return vidas == maxVidas;
+        }
+
+        public void ActualizarPalabra()
+        {
+            bool acierto = false;
+            Console.SetCursorPosition((Console.WindowWidth / 2), (Console.WindowHeight / 2) + 15);
+            Console.Write("Introduce una letra: ");
+            char letraUsuario = Convert.ToChar(Console.ReadLine());
+            for (int i = 0; i < palabras[palabra].Length; i++)
+            {
+                if (palabras[palabra][i] == letraUsuario)
+                {
+                    estado = estado.Remove(i, 1).Insert(i, letraUsuario.ToString());
+                    acierto = true;
+                }
+            }
+            if (!acierto)
+            {
+                AddDaño();
+                ActualizarAhorcado();
+            }
+        }
+
+        public void ActualizarAhorcado()
+        {
+            DibujarAhorcado(GetEstado());
+        }
+
+        public void Jugar()
+        {
+            Random generator = new Random();
+            palabra = generator.Next(0, palabras.Count() + 1);
+            estado = "";
+            for (int i = 0; i < palabras[palabra].Length; i++)
+            {
+                estado += "_";
+            }
+
+            while (!HasGanado() && !EstasAhorcado())
+            {
+                Console.Clear();
+                DibujarPalabra();
+                DibujarAhorcado(GetEstado());
+                
+                ActualizarPalabra();
+                ActualizarAhorcado();
+            }
+            if(HasGanado())
+            {
+                Console.SetCursorPosition((Console.WindowWidth / 2), (Console.WindowHeight / 2) + 20);
+                Console.WriteLine($"¡Has ganado! La palabra era: {palabras[palabra]}");
+            }
+            else
+            {
+                Console.SetCursorPosition((Console.WindowWidth / 2), (Console.WindowHeight / 2) + 20);
+                Console.WriteLine($"¡Has perdido! La palabra era: {palabras[palabra]}");
+            }
+        }
+    }
+}
---
+++ ProyectoAhorcado/ProyectoAhorcado/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Autor: gabhercol
+/* Fecha: 2/18/2025 11:31:55 AM
+/* Descripción:Realizaremos una clase llamada Ahorcado y que nos permitirá jugar al ahorcado.
+
+Esta clase tendrá una lista de palabras que puedan salir para el juego y un entero que será la palabra con 
+la que estamos jugando ahora mismo y un string con el estado de la palabra (--A--) o (H-LA). O sea como está 
+ahora mismo la palabra de destapada. Y un entero con el estado del ahorcado. El getter de esta dato deberá 
+devolver un string indicando: cabeza, cuerpo, brazo, brazos... en función del estado. Y en vez de setter habrá 
+un método AddDaño que le sumará 1 al estado del ahorcado.
+
+Deberá tener también un método Add(string) para añadir nuevas palabras a la lista.
+
+El método jugar() generará un número aleatorio entre 0 y el número de palabras que hay en Lista (Count). Esa será 
+la palabra con la que se va a Jugar. Después se llamará al método DibujaPalabra() que dibuja el estado de la palabra 
+actual y a otro método DibujaAhorcado() que dibujará el muñequito en función de como vamos. Después de esto se pide 
+una letra al usuario y se llama al método ActualizaPalabra() que como su nombre indica actualiza el estado de la palabra 
+y al método ActualizaAhorcado(). Esto se realizará mientras no ganemos (método bool HasGanado() que comprueba el estado
+de la palabra para ver si hemos ganado)  o nos ahorquemos (método EstasAhorcado() que comprueba si no nos quedan oportunidades). 
+*/
+namespace ProyectoAhorcado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] palabras = { "hola", "adios", "casa", "coche", "perro", "gato", "raton", "elefante", "caballo", "pajaro" };
+            List<string> listaPalabras = palabras.ToList();
+
+            Ahorcado ahorcado = new Ahorcado(listaPalabras);
+
+            ahorcado.Jugar();
+        }
+    }
+}
