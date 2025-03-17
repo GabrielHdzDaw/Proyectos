41b1c630b3e5d0bc4789fc7ebf6f0f993f1a1650
gabrielhdzdaw <gabrielhernandezdaw@gmail.com>
2025-03-17 17:05:21 +0100

    17/3/2025

---
+++ ProyectoExtraerTexto/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Autor: gabri
+/* Fecha: 17/03/2025 16:44:34
+/* Descripción: Crea un programa que extraiga el texto imprimible (ASCII 32-126, 10 y 13) de 
+un fichero binario y lo escribirá en un fichero de texto. Siempre comprobando errores.
+*/
+namespace ProyectoExtraerTexto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ruta = @"..\..\..\fichero.bmp";
+			try
+			{
+                FileStream fs = new FileStream(ruta, FileMode.Open);
+                byte[] bytes = new byte[fs.Length];
+                fs.Read(bytes, 0, (int)fs.Length);
+                foreach (byte b in bytes)
+                {
+                    if ((b >= 32 && b <= 126) || b == 10 || b == 13)
+                    {
+                        Console.Write((char)b);
+                    }
+                }
+
+
+            }
+			catch (IOException)
+			{
+                Console.WriteLine("Error al leer el fichero");
+            }
+        }
+    }
+}
